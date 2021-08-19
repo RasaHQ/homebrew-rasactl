fbef2cef77a5a15c463ecfa701c361352e4c906f
rasabot <rasabot@rasa.com>
2021-08-19 13:31:07 +0200

    Brew formula update for rasactl version 0.0.7

--- Formula/rasactl.rb
+++ Formula/rasactl.rb
@@ -11,18 +11,18 @@ class Rasactl < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/RasaHQ/rasactl/releases/download/0.0.7/rasactl_0.0.7_darwin_amd64.tar.gz"
-      sha256 "8b77f550dd3812bfa0c233213b0c4d8e6ba8ee5070832ab5d5ef4c5896e84a9b"
+      sha256 "e7ae7aec261cad6fd45d94925754a9104c32f128b0f9300a807e708dcad9ace0"
     end
     if Hardware::CPU.arm?
       url "https://github.com/RasaHQ/rasactl/releases/download/0.0.7/rasactl_0.0.7_darwin_arm64.tar.gz"
-      sha256 "45417bb4829ceca5362abc01de7fc106f6356a04ef9f454bce89f366803e03c7"
+      sha256 "70da3c4406fe4dcfdef38423d6f1ab5b31d52e8a034ebc7f2d7a971043b6f4ec"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/RasaHQ/rasactl/releases/download/0.0.7/rasactl_0.0.7_linux_amd64.tar.gz"
-      sha256 "7c90f1f4467208bc2fa9331f5e09f4911696aa005a600df58e2132ca1872b277"
+      sha256 "490012ea6aa577f1954348c24fc1e1d1cb3ddccaf15f86acfa24c0e31b441e51"
     end
   end
 
