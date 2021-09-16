d0055d90ae426d10d11113027be2291dea3f3036
rasabot <rasabot@rasa.com>
2021-09-16 12:05:14 +0200

    Brew formula update for rasactl version 0.0.22

--- Formula/rasactl.rb
+++ Formula/rasactl.rb
@@ -5,24 +5,24 @@
 class Rasactl < Formula
   desc "rasactl deploys Rasa X / Enterprise on your local or remote Kubernetes cluster and manages Rasa X / Enterprise deployments."
   homepage "https://github.com/RasaHQ/rasactl"
-  version "0.0.21"
+  version "0.0.22"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/RasaHQ/rasactl/releases/download/0.0.21/rasactl_0.0.21_darwin_amd64.tar.gz"
-      sha256 "995924cec4765b98861e36668a6b8e8733f76a20fb8e16a0344b4486d885aab0"
+      url "https://github.com/RasaHQ/rasactl/releases/download/0.0.22/rasactl_0.0.22_darwin_amd64.tar.gz"
+      sha256 "3c853337b1d2e72bb7be1f01ebf5d32e9c70eb836d231e4f89272c640e5d2439"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/RasaHQ/rasactl/releases/download/0.0.21/rasactl_0.0.21_darwin_arm64.tar.gz"
-      sha256 "ec0dec4b4fd9a49e14755bcc85af313d99d145cf8f0be229be6c90130514981d"
+      url "https://github.com/RasaHQ/rasactl/releases/download/0.0.22/rasactl_0.0.22_darwin_arm64.tar.gz"
+      sha256 "92f79e946bf7c32d79ead252f40fb9173833ba568626d4d288cc5ab0485fa13c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/RasaHQ/rasactl/releases/download/0.0.21/rasactl_0.0.21_linux_amd64.tar.gz"
-      sha256 "b7d1d292e6890316aafeeddb1b3e9842383faa47daf3f560ca11fea7adb91f4d"
+      url "https://github.com/RasaHQ/rasactl/releases/download/0.0.22/rasactl_0.0.22_linux_amd64.tar.gz"
+      sha256 "56a96f3dfe0ab8634c451d4c006885542a274736e90ea7f650f7bd4e39dab899"
     end
   end
 
